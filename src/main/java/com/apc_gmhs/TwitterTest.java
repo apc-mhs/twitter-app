8e6f685c58c802cc156e84c0eee9a3e2d4c8abba
AndrewLester <alester220@gmail.com>
2020-02-16 18:59:15 -0500

    Use twitter4j properties file

--- src/main/java/com/apc_gmhs/TwitterTest.java
+++ src/main/java/com/apc_gmhs/TwitterTest.java
@@ -6,18 +6,10 @@ import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
-import twitter4j.conf.ConfigurationBuilder;
 
 public class TwitterTest {
 	public static void main(String[] args) throws TwitterException {
-		ConfigurationBuilder cb = new ConfigurationBuilder();
-		cb.setDebugEnabled(true)
-		  .setOAuthConsumerKey("*********************")
-		  .setOAuthConsumerSecret("******************************************")
-		  .setOAuthAccessToken("**************************************************")
-		  .setOAuthAccessTokenSecret("******************************************");
-		TwitterFactory tf = new TwitterFactory(cb.build());
-		Twitter twitter = tf.getInstance();
+		Twitter twitter = TwitterFactory.getSingleton();
 		List<Status> statuses = twitter.getUserTimeline("kanyewest");
 	    System.out.println("Showing home timeline.");
 	    for (Status status : statuses) {
