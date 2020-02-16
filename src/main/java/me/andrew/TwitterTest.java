eb86b4f4759077de7cb095f49619ca4c8f567945
AndrewLester <alester220@gmail.com>
2020-02-16 18:54:38 -0500

    Initial commit

---
+++ src/main/java/me/andrew/TwitterTest.java
@@ -0,0 +1,28 @@
+package me.andrew;
+
+import java.util.List;
+
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.conf.ConfigurationBuilder;
+
+public class TwitterTest {
+	public static void main(String[] args) throws TwitterException {
+		ConfigurationBuilder cb = new ConfigurationBuilder();
+		cb.setDebugEnabled(true)
+		  .setOAuthConsumerKey("*********************")
+		  .setOAuthConsumerSecret("******************************************")
+		  .setOAuthAccessToken("**************************************************")
+		  .setOAuthAccessTokenSecret("******************************************");
+		TwitterFactory tf = new TwitterFactory(cb.build());
+		Twitter twitter = tf.getInstance();
+		List<Status> statuses = twitter.getUserTimeline("kanyewest");
+	    System.out.println("Showing home timeline.");
+	    for (Status status : statuses) {
+	        System.out.println(status.getUser().getName() + ":" +
+	                           status.getText());
+	    }
+	}
+}
